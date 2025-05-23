d9ee8a1aa3358c10d0fb9cc85bf21327c1b1ac3a
xime <ximena.asbb@gmail.com>
2025-05-23 11:08:01 -0300

    Update juego.py

--- juego.py
+++ juego.py
@@ -1,5 +1,14 @@
 #UNE
 import random
+
+def darCartas(cantidad, mazo, usuario):
+    contador=0
+    while contador < cantidad:
+        carta = random.choice  (list(mazo.keys()))
+        usuario.append(carta)
+        mazo[carta] -= 1
+        contador +=1
+
 mazo = {
     '0' :4,
     '1' : 4,
@@ -36,25 +45,24 @@ mazo[carta] -= 1
 print(mazo)
 
 if Jugador1[0] == '+2':
-    carta = random.choice(list(mazo.keys()))
-    Computadora.append(carta)
-    mazo[carta] -= 1
-
-    carta = random.choice(list(mazo.keys()))
-    Computadora.append(carta)
-    mazo[carta] -= 1
-
+    darCartas(2, mazo, Computadora)
     print(mazo)
     print(f"Computadora {Computadora}")
 
 if Jugador1[0]=='+4':
-    carta = random.choice(list(mazo.keys()))
-    Computadora.append(carta)
-    mazo[carta] -= 1
-
-    carta = random.choice  (list(mazo.keys()))
-    Computadora.append(carta)
-    mazo[carta] -= 1
-
+    darCartas(4, mazo, Computadora)
     print(mazo)
     print(f"Computadora {Computadora}")
+
+    if Computadora[0] == '+2':
+    darCartas(2, mazo, Jugador1)
+    print(mazo)
+    print(f"jugador1 {Jugador1}")
+
+   if Computadora[0] == '+4':
+    darCartas(2, mazo, Jugador1)
+    print(mazo)
+    print(f"jugador1 {Jugador1}")
+
+
+
